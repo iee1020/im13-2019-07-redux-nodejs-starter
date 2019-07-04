3833d5d34936fe1c766f00265f5b8871ce1f1f65
iee1020 <yl9536@gmail.com>
2019-07-04 20:48:22 +0900

    redux subscribe

--- index.js
+++ index.js
@@ -15,12 +15,15 @@ const counterReducer = (state = initialState, action) => {
 
 const store = createStore(counterReducer);
 
-console.log("initial state", store.getState()); //0
+// store가 reducer를 통해서 업데이트가 되면 console.log를 찍겠다. -> ui를 업데이트(render)할 때 용이
+store.subscribe(() => console.log(store.getState()));
+
+// console.log("initial state", store.getState()); //0
 store.dispatch({ type: "INCREMENT" });
-console.log("After Increment 1", store.getState()); // 1
+// console.log("After Increment 1", store.getState()); // 1
 store.dispatch({ type: "INCREMENT" });
-console.log("After Increment 2", store.getState()); // 2
+// console.log("After Increment 2", store.getState()); // 2
 store.dispatch({ type: "DECREMENT" });
-console.log("After Decrement 1", store.getState()); // 1
+// console.log("After Decrement 1", store.getState()); // 1
 store.dispatch({ type: "DECREMENT" });
-console.log("After Decrement 2", store.getState()); // 0
+// console.log("After Decrement 2", store.getState()); // 0
